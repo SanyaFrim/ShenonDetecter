10e18dcd712ef8740cdfb55b33593facd7390876
SanyaFrim <sasha_romanovpwr_2018@mail.ru>
2026-05-02 19:50:34 +0300

    Add files via upload

---
+++ new_folder/StringAnalyzerMFC/StringAnalyzerMFC.h
@@ -0,0 +1,32 @@
+﻿
+// StringAnalyzerMFC.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CStringAnalyzerMFCApp:
+// Сведения о реализации этого класса: StringAnalyzerMFC.cpp
+//
+
+class CStringAnalyzerMFCApp : public CWinApp
+{
+public:
+	CStringAnalyzerMFCApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CStringAnalyzerMFCApp theApp;
